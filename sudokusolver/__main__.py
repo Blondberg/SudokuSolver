f3ebc2e404e965497b3e20f6decf3d8c3b75ca3b
Blondberg <iamblondberg@gmail.com>
2019-09-13 21:18:14 +0200

    Created most of the helper functions for board. Refactored some things

--- sudokusolver/__main__.py
+++ sudokusolver/__main__.py
@@ -1,8 +1,22 @@
 # main.py - makes it possible to run the program from terminal with
 # python3 -m sudokusolver
 
-from sudokusolver import solver
+from .common.board import Board
+
+from sudokusolver.solver import solve
+
+import sudokusolver.common.messenger as msgr
+
+# The program starts from here
+def run():
+    msgr.info("Welcome to the python Sudoku Solver!")
+
+    board = Board()
+
+    board.print()
+
+    solve(board)
 
 
 if __name__ == '__main__':
-    solver.run()
+    run()
--- sudokusolver/common/board.py
+++ sudokusolver/common/board.py
@@ -2,7 +2,7 @@
 
 import sudokusolver.common.messenger as msgr
 from .test_boards import TOP_ROW1
-
+from .helpers import *
 
 class Board(object):
 
@@ -21,7 +21,7 @@ class Board(object):
         for row in range(self.size):
             print("|", end='')
             for col in range(self.size):
-                print(" " + str(self.board[row][col]).replace('0', '□') + " ", end='')
+                print(" " + str(self.board[row][col]).replace('0', '_') + " ", end='')
 
                 if (col + 1) % 3 == 0:
                     print("|", end='')
@@ -36,14 +36,25 @@ class Board(object):
         for row in range(self.size):
             print(self.board[row])
 
+
+    # Get chosen row
+    def get_row(self, n):
+        return self.board[n - 1].copy()
+
+    # Get chosen col
+    def get_col(self, n):
+        col = []
+        for row in self.board:
+            col.append(row[n - 1])
+        return col
+
     # Print row of choice, not a good looking output
     def print_row(self, n):
-        print(str(self.board[n - 1]).replace('0', '□ '))
+        print(self.get_row(n).replace('0', '_ '))
 
     # Print col of choice, not a good looking output
     def print_col(self, n):
-        for row in range(self.size):
-            print(str(self.board[row][n - 1]).replace('0', '□'))
+        print(self.get_row(n).replace('0', '_'))
 
     # Get value from chosen position
     def get_value(self, col, row):
@@ -58,15 +69,12 @@ class Board(object):
             msgr.error("The coordinate needs to be a tuple, (col, row)!")
 
     # Check if value v exists in row
-    def value_in_row(self, row, v):
-        return v in self.board[row - 1]
+    def value_in_row(self, n, v):
+        return v in self.get_row(row)
 
     # Check if value v exists in col
-    def value_in_col(self, col, v):
-        for row in self.board:
-            if v == row[col - 1]:
-                return True
-        return False
+    def value_in_col(self, n, v):
+        return v in self.get_col(col)
 
     # Check if value v exists in square sqr_n
     def value_in_square(self, sqr_n, v):
@@ -74,3 +82,25 @@ class Board(object):
         for i in range((sqr_n - 1) * 3):
             print(i)
 
+    # Check if row is correct
+    def validate_row(self, n):
+        return all_unique(self.get_row(n))
+
+    # Check if col is correct
+    def validate_col(self, n):
+        return all_unique(self.get_col(n))
+
+    # check if a row is full
+    def row_full(self, n):
+        return not 0 in self.get_row(n) and all_unique(self.get_row(n))
+
+    # check if a col is full
+    def col_full(self, n):
+        return not 0 in self.get_col(n) and all_unique(self.get_col(n))
+
+    # Check if the board is solved
+    def is_solved(self, board):
+        for i in range(board.size):
+            if(not board.row_full(i)):
+                return False
+        return True

da8062dde09ce088f753a558ba5f16db6e04c7ef
Simon <simbr157@liu.se>
2020-01-19 22:39:18 +0100

    Working on recursion. Close to a solution, doesn't move to next position though

--- sudokusolver/__main__.py
+++ sudokusolver/__main__.py
@@ -3,28 +3,59 @@
 # from sudokusolver import solver
 # put solver.run() in main to run
 
-
-def solve(board):
-    rec_solve(board, 0, 0)
-
-
-def rec_solve(board, row, col):
-    for i in range(0, 9):
-        if is_valid(board, row, col, i):
-            print("tset")
-
-
-def is_valid(board, row, col, n):
-    return in_square(board, get_square(board, row, col), n) or (
-        in_row(board, row, n) or
-        in_col(board, col, n))
+board = [[0, 0, 0, 2, 6, 0, 7, 0, 1],
+         [6, 8, 0, 0, 7, 0, 0, 9, 0],
+         [1, 9, 0, 0, 0, 4, 5, 0, 0],
+         [8, 2, 0, 1, 0, 0, 0, 4, 0],
+         [0, 0, 4, 6, 0, 2, 9, 0, 0],
+         [0, 5, 0, 0, 0, 3, 0, 2, 8],
+         [0, 0, 9, 3, 0, 0, 0, 7, 4],
+         [0, 4, 0, 0, 5, 0, 0, 3, 6],
+         [7, 0, 3, 0, 1, 8, 0, 0, 0]]
 
 
-def get_square(board, row, col):
+def solve():
+    print("Trying to solve board...")
+    rec_solve(0, 0)
+
+
+def rec_solve(row, col):
+    for i in range(1, 9):  # Loop through all possible numbers
+        if is_valid(row, col, i):
+            board[row][col] = i
+            print_board()
+            next_row, next_col = get_next_coord(row, col)
+            if next_row == -1 and next_col == -1:  # we have reached the end
+                return True
+            if not rec_solve(next_row, next_col):
+                board[row][col] = 0
+
+    return False  # return false if no one of the numbers work
+
+
+def get_next_coord(row, col):
+    next_row = row + 1
+    next_col = col
+    if next_row == 9:
+        next_row = 0
+        next_col += 1
+    if next_col == 9:
+        return -1, -1
+
+    return next_row, next_col
+
+
+def is_valid(row, col, n):
+    return not (in_square(get_square(row, col), n) or (
+        in_row(row, n) or
+        in_col(col, n))) and board[row][col] == 0
+
+
+def get_square(row, col):
     return (row // 3) * 3 + col // 3
 
 
-def in_square(board, square, n):
+def in_square(square, n):
     row = square // 3
     col = square % 3
     for i in range(row, row + 3):
@@ -34,25 +65,26 @@ def in_square(board, square, n):
     return False
 
 
-def in_row(board, row, n):
+def in_row(row, n):
     return n in board[row]
 
 
-def in_col(board, col, n):
+def in_col(col, n):
     for row in board:
         if row[col] == n:
             return True
     return False
 
 
+def print_board():
+    print("####")
+    for row in board:
+        print(row)
+    print("####")
+
+
 if __name__ == '__main__':
     # Solve the sudoku
-    solve([[0, 0, 0, 2, 6, 0, 7, 0, 1],
-           [6, 8, 0, 0, 7, 0, 0, 9, 0],
-           [1, 9, 0, 0, 0, 4, 5, 0, 0],
-           [8, 2, 0, 1, 0, 0, 0, 4, 0],
-           [0, 0, 4, 6, 0, 2, 9, 0, 0],
-           [0, 5, 0, 0, 0, 3, 0, 2, 8],
-           [0, 0, 9, 3, 0, 0, 0, 7, 4],
-           [0, 4, 0, 0, 5, 0, 0, 3, 6],
-           [7, 0, 3, 0, 1, 8, 0, 0, 0]])
+    solve()
+
+    print_board()
